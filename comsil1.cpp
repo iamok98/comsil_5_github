94cb3bad46de4ecad3744ee97f5e83434ab26d2c
unknown <qh4321@naver.com>
2022-09-20 23:41:09 +0900

    solve conflict

--- comsil1.cpp
+++ comsil1.cpp
@@ -1,24 +1,12 @@
 ﻿// c프수업.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 //
 
-#include <iostream>
-#define N 10
+#include <stdio.h>
 
 int main()
 {
-    int count;
-    float sum, average, number;
-    sum = 0;
-    count = 0;
-    while (count < N)
-    {
-        scanf_s("%f", &number);
-        sum = sum + number;
-        count = count + 1;
-    }
-    average = sum / N;
-    printf("N=%d Sum=%f", N, sum);
-    printf("Average =%f",average);
+	printf("comsil1 testbranch1\n");
+	return 0;
 }
 
 // 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
